22875a863fcdafe337bddf16be69ae7b6673c6f6
Dezani <henrique.dezani@fatecriopreto.edu.br.local>
2026-03-31 07:47:38 -0300

    Projeto inicial

---
+++ lib/login.dart
@@ -0,0 +1,50 @@
+import 'package:flutter/material.dart';
+
+class LoginPage extends StatelessWidget {
+  const LoginPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Material(
+      child: Container(
+        margin: EdgeInsets.all(20),
+        child: Column(
+          spacing: 16,
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            TextField(
+              decoration: InputDecoration(
+                // filled: true,
+                // fillColor: Colors.blue[100],
+                // border: InputBorder.none
+                border: OutlineInputBorder(),
+                labelText: "E-mail",
+                prefixIcon: Icon(Icons.email_outlined),
+              ),
+            ),
+            TextField(
+              obscureText: true,
+              decoration: InputDecoration(
+                border: OutlineInputBorder(),
+                labelText: "Senha",
+                prefixIcon: Icon(Icons.password_outlined),
+                suffixIcon: IconButton(
+                  onPressed: () {}, // Exercício.
+                  icon: Icon(Icons.remove_red_eye_sharp),
+                )
+              ),
+            ),
+            ElevatedButton(
+              onPressed: () {},
+              child: Text("Entrar"),
+            ),
+            TextButton(
+              onPressed: () {},
+              child: Text("Registrar"),
+            ),
+          ]
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/registro.dart
@@ -0,0 +1,57 @@
+import 'package:flutter/material.dart';
+
+class RegistroPage extends StatelessWidget {
+  const RegistroPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Material(
+      child: Container(
+        margin: EdgeInsets.all(20),
+        child: Column(
+          spacing: 16,
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            TextField(
+              decoration: InputDecoration(
+                border: OutlineInputBorder(),
+                labelText: "Nome",
+                prefixIcon: Icon(Icons.text_fields_outlined),
+              ),
+            ),
+            TextField(
+              decoration: InputDecoration(
+                // filled: true,
+                // fillColor: Colors.blue[100],
+                // border: InputBorder.none
+                border: OutlineInputBorder(),
+                labelText: "E-mail",
+                prefixIcon: Icon(Icons.email_outlined),
+              ),
+            ),
+            TextField(
+              obscureText: true,
+              decoration: InputDecoration(
+                border: OutlineInputBorder(),
+                labelText: "Senha",
+                prefixIcon: Icon(Icons.password_outlined),
+                suffixIcon: IconButton(
+                  onPressed: () {}, // Exercício.
+                  icon: Icon(Icons.remove_red_eye_sharp),
+                )
+              ),
+            ),
+            ElevatedButton(
+              onPressed: () {},
+              child: Text("Registrar"),
+            ),
+            TextButton(
+              onPressed: () {},
+              child: Text("Voltar"),
+            ),
+          ]
+        ),
+      ),
+    );
+  }
+}
