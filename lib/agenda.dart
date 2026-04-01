6b68bee0703a85ae9499d136bdce265dd8452cd0
Dezani <henrique.dezani@fatecriopreto.edu.br.local>
2026-04-01 08:59:11 -0300

    telas finais

--- lib/agenda.dart
+++ lib/agenda.dart
@@ -1,10 +1,20 @@
 import 'package:flutter/material.dart';
 
 class AgendaPage extends StatelessWidget {
-  const AgendaPage({super.key});
 
   @override
   Widget build(BuildContext context) {
+    DateTime dataAtual = DateTime.now();
+    DateTime dataFinal = dataAtual.add(Duration(days: 45));
+
+    
+    List<int> dias = [];
+    DateTime data = dataAtual;
+    while (data.isBefore(dataFinal)) {
+      dias.add(data.day);
+      data = data.add(Duration(days: 1)); 
+    }
+
     return Scaffold(
       appBar: AppBar(
         title: Text("Agenda"),
@@ -13,11 +23,48 @@ class AgendaPage extends StatelessWidget {
             onPressed: () => Navigator.pushReplacementNamed(context, "/login"),
             icon: Icon(Icons.logout_outlined),
           ),
-        ]
+        ],
       ),
       floatingActionButton: FloatingActionButton(
         onPressed: () => Navigator.pushNamed(context, "/novo"),
         child: Icon(Icons.add),
+      ),
+      body: Column(
+        children: [          
+          Container(
+            height: 40,
+            color: Colors.blue[100],
+            child: ListView(
+              scrollDirection: Axis.horizontal,
+              children: [
+                for(int dia in dias)
+                GestureDetector(
+                  onTap: () {}, // TODO: Carrega agenda do dia
+                  child: Container(
+                    padding: EdgeInsets.all(6),
+                    margin: EdgeInsets.only(right: 6),
+                    decoration: BoxDecoration(
+                      color: Colors.blue[600],
+                      shape: BoxShape.circle,
+                    ),
+                    child: Text(
+                      dia.toString().padLeft(2, "0"),
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ),
+          ListTile(
+            leading: Icon(Icons.calendar_month),
+            title: Text("Dentista"),
+            subtitle: Text("01/04/2026 às 14h00"),
+          ),
+        ],
       ),
     );
   }
